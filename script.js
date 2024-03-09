edc1b05a0a4e4919b6ba3639eb543a104899b14d
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2024-03-10 00:35:39 +0300

    ...

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 'use strict';
 
-(function modalComponent() {
-    const modal = document.querySelector('.modal');
-    const overlay = document.querySelector('.overlay');
-    const btnCloseModal = document.querySelector('.btn--close-modal');
-    const btnOpenModals = document.querySelectorAll('.btn--show-modal');
-
-    const openModal = function (evt) {
-        evt.preventDefault();
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    };
-
-    const closeModal = function () {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    };
-
-    btnOpenModals.forEach(btn => {
-        btn.addEventListener('click', openModal);
-    });
-
-    btnCloseModal.addEventListener('click', closeModal);
-    overlay.addEventListener('click', closeModal);
-
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-            closeModal();
-        }
-    });
-})();
-
 (function tabComponent() {
     const tabContainer = document.querySelector('.services__tab-container');
     const tabs = document.querySelectorAll('.services__tab');
